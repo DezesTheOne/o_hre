f1d225d4d4de867547c285180a6b10ddd2efd9e7
Hedvika Reichlová <hedvika.reichlova@gmail.com>
2023-11-03 14:26:40 +0100

    layout

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,13 +5,21 @@ import './index.css';
 document.querySelector('#root').innerHTML = render(
   <div className="container">
     <header>
-      <h1>O projektu</h1>
+      <h4 className='oprojektu'> <a href='./index.html'> O projektu </a> </h4>
+      <div className='team'>
+        <h4> <a href=''> Hedvika Reichlová </a> </h4>
+        <h4> <a href=''> Tereza Smolová </a> </h4>
+        <h4> <a href=''> Kristýna Jozífová </a> </h4>
+      </div>
     </header>
     <main>
       <p></p>
     </main>
     <footer>
-      <p>Hedvika Reichlová, Tereza Smolová, Kristýna Jozífová - Czechitas, Digitální akademie: Web</p>
+      <p>
+        Hedvika Reichlová, Tereza Smolová, Kristýna Jozífová - Czechitas,
+        Digitální akademie: Web
+      </p>
     </footer>
-  </div>
+  </div>,
 );
