f672b4eed4f8764bac106e393f3483ee0f32136b
Hedvika Reichlová <hedvika.reichlova@gmail.com>
2023-11-03 11:38:32 +0100

    main

---
+++ src/pages/index.jsx
@@ -0,0 +1,18 @@
+import { render } from '@czechitas/render';
+import '../global.css';
+import './index.css';
+
+document.querySelector('#root').innerHTML = render(
+  <div className="container">
+    <header>
+      <div className="logo"></div>
+      <h1>Webová aplikace</h1>
+    </header>
+    <main>
+      <p>Startovací šablona pro webovou aplikaci v JavaScriptu s JSX. Vytvořeno pomocí <a href="https://www.npmjs.com/package/create-kodim-app">create-kodim-app</a>.</p>
+    </main>
+    <footer>
+      <p>Czechitas, Digitální akademie: Web</p>
+    </footer>
+  </div>
+);
